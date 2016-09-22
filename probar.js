601d2b0802766f55029c339d460345c36a2b1b83
Alejandro Di Battista <alejandrodibattista@gmail.com>
2016-09-22 15:05:48 -0300

    Eliminamos Archivos de prueba

--- probar.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-var h = [1, 2, 3]
-console.log(h)
-console.log(h[0] || null)
-console.log(h[5] || null)
--- semaforo.js
+++
@@ -1,9 +0,0 @@
-{
-  s0001: {
-    ubicacion: "aa",
-    verde1: null,
-    rojo1: null,
-    verde2: null,
-    rojo2: null,
-  }
-}
